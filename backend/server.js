27533b6895551f0f4ebaf2a4e063b7c8dfe78e5b
reachoutsahani <ajaysahani6392@gmail.com>
2026-05-11 03:03:13 +0530

    fixed cors for vercel domain

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://foodrush-self.vercel.app',
+    'https://foodrush-seven.vercel.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
@@ -72,6 +73,7 @@ const io = new Server(server, {
     origin: [
       'http://localhost:5173',
       'https://foodrush-self.vercel.app',
+      'https://foodrush-seven.vercel.app',
     ],
     methods: ['GET', 'POST'],
     credentials: true,
